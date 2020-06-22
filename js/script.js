434a520e2e29f9efbf0bc3b0265de59397dd8c9c
Unknown <fitrakun@gmail.com>
2020-06-22 21:21:24 +0900

    Add bootstrap suggest for mention

--- js/script.js
+++ js/script.js
@@ -96,6 +96,67 @@ $(document).ready(function() {
     $('.reply-comment').one('click tap', function() {
         $(this).closest('.media-body').append('<div class = "mt-3 media position-relative"><img src = "https://images.unsplash.com/photo-1554080353-a576cf803bda?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80" alt = "Fitra Rahmamuliani Avatar" class = "mr-3 rounded-circle comment-photo" /> <div class = "media-body align-self-center"> <form class = "d-flex flex-row mb-0 reply-comment-form" id="reply-form"><input type = "text" class = "form-control flex-grow-1 reply-comment-input" placeholder = "Write a reply..." /> <button type = "button" class = "btn btn-primary ml-2 reply-comment-submit" > COMMENT </button> </form> </div> </div>');
         $('.reply-comment-input').focus();
+        $('.reply-comment-input').suggest('@', {
+            data: memberList,
+            map: function(user) {
+                return {
+                    value: user.name,
+                    text: '<strong>' + user.name + '</strong>'
+                }
+            }
+        });
+    });
+
+    var memberList = [
+        { name: "Xiangshi Ren" },
+        { name: "Kiyoshi Nakahara" },
+        { name: "Kaechang Park" },
+        { name: "Yukinobu Hoshino" },
+        { name: "Kazunori Ueda" },
+        { name: "Toru Kurihara" },
+        { name: "Kavous Salehzadeh Niksirat" },
+        { name: "Silpasuwanchai Chaklam" },
+        { name: "Kibum Kim" },
+        { name: "Zhenxin Wang" },
+        { name: "Sayan Sarcar" },
+        { name: "William Delamare" },
+        { name: "Hamed Aliyari" },
+        { name: "Kyoko Hatakenaka" },
+        { name: "Xinhui Jiang" },
+        { name: "Yang Li" },
+        { name: "Chen Wang" },
+        { name: "Fitra Rahmamuliani" },
+        { name: "Xiaoxuan Li" },
+        { name: "Yilin Zheng" },
+        { name: "Chunyuan Lan" },
+        { name: "Xinpeng Li" },
+        { name: "Xi Chen" },
+        { name: "Sai Jiang" },
+        { name: "Hongyun Lyu" },
+        { name: "Jian Zhang" },
+        { name: "Zhihang Guo" },
+        { name: "Yanyin Zhou" },
+        { name: "Xiaofei Zhu" },
+        { name: "Junlin Sun" },
+        { name: "Akinori Kondo" },
+        { name: "Hijiri Kaneko" },
+        { name: "Ryota Torii" },
+        { name: "Takaaki Kubo" },
+        { name: "Yusuke Tokito" },
+        { name: "Saki Hiramatsu" },
+        { name: "Jiayuan Geng" },
+        { name: "Adachi Kenshi" },
+        { name: "Miyamoto Daisuke" }
+    ];
+
+    $('.input-comment').suggest('@', {
+        data: memberList,
+        map: function(user) {
+            return {
+                value: user.name,
+                text: '<strong>' + user.name + '</strong>'
+            }
+        }
     });
 
     $(document).on("keypress", ".reply-comment-input", function(e) {
